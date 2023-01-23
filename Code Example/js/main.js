338aaaa615d9e0f5f4caf2cefdf3fc0bba6689c5
VinnieChachra <vinniechachra@gmail.com>
2023-01-23 18:33:39 -0500

    Play with CSS and some JS

---
+++ Code Example/js/main.js
@@ -0,0 +1,8 @@
+console.log("Script loaded");
+console.log("Hello World")
+
+button = document.querySelector("button");
+button.onclick = () => {
+    console.log("YOU CLICKED ME")
+    Window.location.href = ("https://google,com")
+};
